8079cd0ba4d2824a9707e3a2e2641b69a6baaa3b
Angel Bernardino <chiv_157@hotmail.com>
2026-04-28 18:55:56 -0600

    MouseC

--- GA_ParticleSyst/Assets/Script/MouseColor.cs
+++ GA_ParticleSyst/Assets/Script/MouseColor.cs
@@ -4,20 +4,18 @@ using TMPro;
 
 public class MouseColor : MonoBehaviour
 {
-    /*public Material material;
-    public TextMeshProGUI textoRGB;
+    public Material material;
+    public TextMeshProUGUI textoRGB;
     private Camera cam;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = Camera.main;
-        material= GetComponent<Render>().material;
+        material= GetComponent<Renderer>().material;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Mouse.current = null) return;
+        if (Mouse.current == null) return;
 
         Vector2 MousePos = Mouse.current.position.ReadValue();
         Ray ray=cam.ScreenPointToRay(MousePos);
@@ -31,11 +29,12 @@ public class MouseColor : MonoBehaviour
             float g = (local.y + 0.5f);
             float b = (local.z + 0.5f);
 
+            Color color = new Color(r, g, b, 1.0f);
             material.SetColor("_Color", color);
             if (textoRGB != null)
             {
                 textoRGB.text = $"R:F2{r:F2}G:{g:F2}B:{b:F2}";
             }
         }
-    }*/
+    }
 }
